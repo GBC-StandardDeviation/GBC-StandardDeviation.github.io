3eb1c29c8cd676f7c118db942e7d7f847540f401
NikolaKordic <39223710+NikolaKordic@users.noreply.github.com>
2018-07-26 18:36:46 -0400

    commit

---
+++ javascript/inventory.js
@@ -0,0 +1,25 @@
+
+var can1=document.getElementById("inventory");
+var ctx=can1.getContext("2d");
+ctx.font = "12px Georgia";
+ctx.fillStyle = "black";
+ctx.fillText("Item| Description",4,12);
+
+var tBox=document.getElementById("textBox");
+var txt=tBox.getContext("2d");
+
+var itemImg = new Image();//key image
+itemImg.src = "images/item/1.png";
+
+function btn1() { //use this function to call out inventory window.
+
+    if(can1.style.display=="none"){
+            can1.style.display="block";
+            can1.style.background="green";
+        }
+        else {
+            can1.style.display="none";
+        }
+}
+
+
